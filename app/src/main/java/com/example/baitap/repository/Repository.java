dc46647361f87246f63a3e95a180065bdf97d33b
Admin <anhnguyentuan07@gmail.com>
2022-03-29 16:26:54 +0700

    tuan anh : update

--- app/src/main/java/com/example/baitap/repository/Repository.java
+++ app/src/main/java/com/example/baitap/repository/Repository.java
@@ -16,27 +16,17 @@ import retrofit2.Call;
 
 public class Repository {
     private UserDatabase userDatabase;
-    private LiveData<List<User>> users;
     private UserDao userDao;
     private UserApi userApi;
 
     public Repository(Application application) {
         userDatabase = UserDatabase.getInstance(application);
         userDao = userDatabase.userDAO();
-        users = userDao.getUsers();
         userApi = RetrofitClient.getInstance().create(UserApi.class);
-    }
-
-    public LiveData<List<User>> getUsers() {
-        return users;
     }
 
     public void insertUsers(List<User> users) {
         userDao.insertUsers(users);
-    }
-
-    public void delete(){
-        userDao.deleteAll();
     }
 
     public Call<List<User>> requestUsers(){
